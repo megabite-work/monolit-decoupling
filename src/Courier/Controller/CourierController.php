5c28e86271a16b0eab616db7357606f32f494154
megabite-work <g.work.01.01.2000@gmail.com>
2024-12-03 21:01:56 +0500

    add service clients

--- src/Courier/Controller/CourierController.php
+++ src/Courier/Controller/CourierController.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App\Courier\Controller;
 
+use App\Common\Client\CustomerServiceClient;
 use App\Courier\Dto\ChangeDeliveryStatusDto;
 use App\Courier\Service\CourierService;
-use App\Customer\Service\CustomerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
@@ -18,10 +18,10 @@ class CourierController extends AbstractController
     public function __construct(private readonly CourierService $courierService) {}
 
     #[Route('/delivery', methods: 'PATCH')]
-    public function changeDeliveryStatus(#[MapRequestPayload] ChangeDeliveryStatusDto $dto, CustomerService $customerService): JsonResponse
+    public function changeDeliveryStatus(#[MapRequestPayload] ChangeDeliveryStatusDto $dto, CustomerServiceClient $customerServiceClient): JsonResponse
     {
         $delivery = $this->courierService->changeDeliveryStatus($dto);
-        $customerService->changeOrderStatus($delivery->getRelatedOrderId(), $delivery->getStatus());
+        $customerServiceClient->changeOrderStatus($delivery->getRelatedOrderId(), $delivery->getStatus());
         
         return $this->json(
             data: $delivery,
--- src/Customer/Service/CustomerService.php
+++ src/Customer/Service/CustomerService.php
@@ -4,33 +4,36 @@ declare(strict_types=1);
 
 namespace App\Customer\Service;
 
+use App\Common\Client\CourierServiceClient;
+use App\Common\Client\RestaurantServiceClient;
+use App\Common\Dto\Order as OrderDto;
 use App\Common\Exception\ErrorException;
-use App\Courier\Service\CourierService;
 use App\Customer\Dto\CreateOrderDto;
 use App\Customer\Entity\Order;
-use App\Restaurant\Service\RestaurantService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 readonly class CustomerService
 {
     public function __construct(
-        private RestaurantService $restaurantService,
-        private CourierService $deliveryService,
+        private RestaurantServiceClient $restaurantServiceClient,
+        private CourierServiceClient $courierServiceClient,
         private EntityManagerInterface $customerEntityManager
     ) {}
 
     public function createOrder(CreateOrderDto $dto): Order
     {
-        $restaurant = $this->restaurantService->getRestaurant($dto->getRestaurantId());
+        $restaurant = $this->restaurantServiceClient->getRestaurant($dto->getRestaurantId());
         $order = (new Order())
             ->setRestaurantId($restaurant->getId())
             ->setStatus(Order::STATUS_NEW);
 
-        if ($this->restaurantService->acceptOrder($order->getId())) {
+        $orderDto = new Orderdto($order->getId(), $order->getStatus(), $order->getRestaurantId(), $order->getDeliveryId());
+            
+        if ($this->restaurantServiceClient->acceptOrder($orderDto)) {
             $order->setStatus(Order::STATUS_ACCEPTED);
-            $newDelivery = $this->deliveryService->createDelivery($order->getId());
-            $order->setDeliveryId($newDelivery->getId());
+            $delivery = $this->courierServiceClient->createDelivery($orderDto);
+            $order->setDeliveryId($delivery->getId());
         } else {
             $order->setStatus(Order::STATUS_DECLINED);
         }

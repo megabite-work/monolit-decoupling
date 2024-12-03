43ac11eb9af6f83bdc10b3028d9828d358c9fbcf
megabite-work <g.work.01.01.2000@gmail.com>
2024-12-03 16:41:54 +0500

    add event listener attribute

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -4,22 +4,29 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
+use App\Exception\ErrorException;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
-#[AsEventListener('kernel.exception')]
+#[AsEventListener(event: 'kernel.exception')]
 class ExceptionListener
 {
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
+
+        if (!$exception instanceof ErrorException) {
+            return;
+        }
+
         $response = new JsonResponse(
             data: [
                 'success' => false,
                 'message' => $exception->getMessage(),
             ],
-            status: $exception->getCode() ?? 500,
+            status: $exception->getCode() ?: Response::HTTP_INTERNAL_SERVER_ERROR,
         );
 
         $event->setResponse($response);
